144801ad546b5391a2852d230fcebb5d73c65a2f
ryulkim <hobby519@naver.com>
2025-11-10 20:44:50 +0900

    feat(input): 재입력 기능 추가

--- src/main/java/racingcar/common/Exception.java
+++ src/main/java/racingcar/common/Exception.java
@@ -3,12 +3,14 @@ package racingcar.common;
 public enum Exception {
     EXCEED_CAR_NAME("자동차 이름은 5자리 이하여야 합니다."),
     DUPLICATE_CAR_NAME("중복된 자동차 이름이 있습니다"),
-    NOT_UNDER_ZERO("시도 횟수가 0 이하일 수는 없습니다."),
+    NOT_UNDER_ZERO("시도 횟수가 1~20 사이여야 합니다."),
     NOT_NUMBER("정수가 아닌 입력값을 받았습니다."),
     EXCEED_CAR_SPEED("스피드 범위에 맞지 않습니다."),
     EXCEED_CAR_POWER("파워 범위에 맞지 않습니다."),
     EXCEED_CAR_LUCK("최대 운빨 범위에 맞지 않습니다."),
-    NO_CAR_TYPE("없는 차 종류입니다.");
+    NO_CAR_TYPE("없는 차 종류입니다."),
+    NOT_MODE("없는 모드입니다. 1~2 중에 선택해주세요."),
+    NOT_SINGLE_MODE("없는 모드입니다. 1~3 중에 선택해주세요.");
     private final String message;
 
     Exception(String message) {
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,42 +1,89 @@
 package racingcar.view;
 
+import static racingcar.common.Exception.NOT_MODE;
+import static racingcar.common.Exception.NOT_SINGLE_MODE;
+import static racingcar.common.Exception.NOT_UNDER_ZERO;
 import static racingcar.common.Message.INPUT_CAR_NAME;
 import static racingcar.common.Message.INPUT_NUM;
 import static racingcar.common.Message.SELECT_MODE;
 import static racingcar.common.Message.SELECT_SINGLE_MODE;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.function.Supplier;
 import racingcar.util.InputParser;
 import racingcar.util.Print;
 
 public class InputView {
 
     public static int inputMode() {
-        Print.println(SELECT_MODE);
-        return InputParser.parseInt(readLine());
+        return retryOnException(() -> {
+            Print.println(SELECT_MODE);
+            return validMode(readLine());
+        });
     }
 
     public static int inputSingleMode() {
-        Print.println(SELECT_SINGLE_MODE);
-        return InputParser.parseInt(readLine());
+        return retryOnException(() -> {
+            Print.println(SELECT_SINGLE_MODE);
+            return validSingleMode(readLine());
+        });
     }
 
     public static int inputNum() {
-        Print.println(INPUT_NUM);
-        return InputParser.parseInt(readLine());
+        return retryOnException(() -> {
+            Print.println(INPUT_NUM);
+            return validNum(readLine());
+        });
     }
 
     public static String[] inputCarNames() {
-        Print.println(INPUT_CAR_NAME);
-        return InputParser.parseCarNames(readLine());
+        return retryOnException(() -> {
+            Print.println(INPUT_CAR_NAME);
+            return InputParser.parseCarNames(readLine());
+        });
     }
 
     public static void close() {
         Console.close();
     }
 
+    private static int validMode(String mod) {
+        int num = InputParser.parseInt(mod);
+        if (num < 1 || num > 2) {
+            throw new IllegalArgumentException(NOT_MODE.toString());
+        }
+        return num;
+    }
+
+    private static int validSingleMode(String mod) {
+        int num = InputParser.parseInt(mod);
+        if (num < 1 || num > 3) {
+            throw new IllegalArgumentException(NOT_SINGLE_MODE.toString());
+        }
+        return num;
+    }
+
+    private static int validNum(String num) {
+        int number = InputParser.parseInt(num);
+        if (number <= 0 || number > 20) {
+            throw new IllegalArgumentException(NOT_UNDER_ZERO.toString());
+        }
+        return number;
+    }
+
     private static String readLine() {
         return Console.readLine();
+    }
+
+    private static <T> T retryOnException(Supplier<T> method) {
+        while (true) {
+            try {
+                return method.get();
+            } catch (IllegalArgumentException e) {
+                Print.println(e.getMessage());
+                Print.println("");
+            }
+        }
     }
 
 }
