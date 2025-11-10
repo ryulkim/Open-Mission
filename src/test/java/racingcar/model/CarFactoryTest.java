7befbd4d804e3949703d2ddbace015cd8b8a37aa
ryulkim <hobby519@naver.com>
2025-11-10 19:51:10 +0900

    test(CarFactory): 속성 값이 자동차의 속성 범위를 넘어갈 경우 오류 테스트

--- src/test/java/racingcar/model/CarFactoryTest.java
+++ src/test/java/racingcar/model/CarFactoryTest.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.common.CarSpec;
 
@@ -19,5 +20,27 @@ public class CarFactoryTest {
         Assertions.assertEquals(carSpec, car.getCarSpec());
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "DEFAULT, -10, 0, 0",
+            "DEFAULT, 0, 101, 0",
+            "DEFAULT, 0, 0, 102",
+            "TROLL, -101, 0, 0",
+            "TROLL, 0, 100000, 0",
+            "TROLL, 0, 0, 100000",
+            "TRUCK, 100, 0, 0",
+            "TRUCK, 0, 200000, 0",
+            "TRUCK, 0, 0, -10",
+    })
+    public void 오류_CAR_최소_최대_범위_아닐_경우(String carType, int speed, int power, int luck) {
+        // given
+        CarSpec carSpec = CarSpec.valueOf(carType);
+
+        // when
+
+        // then
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> CarFactory.createCar(carSpec, speed, power, luck));
+    }
 
 }
