345c14090efe21238529d141e034b70d007d8f04
ryulkim <hobby519@naver.com>
2025-11-10 19:18:32 +0900

    refactor(carFactory): car factory 리팩토링

--- src/main/java/racingcar/model/CarFactory.java
+++ src/main/java/racingcar/model/CarFactory.java
@@ -2,15 +2,20 @@ package racingcar.model;
 
 import static racingcar.common.Exception.NO_CAR_TYPE;
 
+import racingcar.common.CarSpec;
+
 public final class CarFactory {
-    public static Car createCar(String carType, int speed, int power, int maxLuck) {
-        if ("DefaultCar".equals(carType)) {
+    public static Car createCar(CarSpec carType, int speed, int power, int maxLuck) {
+        if (carType.equals(CarSpec.DEFAULT)) {
             return new DefaultCar(speed, power, maxLuck);
         }
-        if ("Truck".equals(carType)) {
+        if (carType.equals(CarSpec.TRUCK)) {
             return new Truck(speed, power, maxLuck);
         }
-        throw new IllegalArgumentException(NO_CAR_TYPE + carType);
+        if (carType.equals(CarSpec.TROLL)) {
+            return new Truck(speed, power, maxLuck);
+        }
+        throw new IllegalArgumentException(NO_CAR_TYPE.toString());
     }
 
 }
